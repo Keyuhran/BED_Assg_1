0e403c0355c82d471df4273d389376c36beb1876
rottenpastaz <johnyonghaiwen@gmail.com>
2024-06-21 22:57:28 +0800

    working on login and maybe signup page added back acceditor, prodeditor,sellorhomeditor js's adn changed all the routings

--- javascript/script.js
+++ javascript/script.js
@@ -10,3 +10,13 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+$(window).on('resize', function() {
+    if($(window).width() > 600) {
+        $('#body').addClass('limit1200');
+        $('#body').removeClass('limit400');
+    }
+    else {
+        $('#body').addClass('limit400');
+        $('#body').removeClass('limit1200');
+    }
+})
