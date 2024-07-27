6c92dab47584f0a86304534ec6515733487b3410
Pofrzyzz <148027130+Pofrzyzz@users.noreply.github.com>
2024-07-28 01:32:58 +0800

    adjusted review.js

--- models/review.js
+++ models/review.js
@@ -32,8 +32,86 @@ class Review {
     static async delReview(email){
         const connection = await sql.connect(dbConfig);
         const sqlQuery = `
-        DELETE FROM Reviews where email = 
-        `
+        DELETE FROM Reviews where email = @Email
+        `;
+
+        const request = connection.request();
+        request.input("Email", sql.VarChar, email);
+
+        const result = await request.query(sqlQuery);
+        connection.close();
+
+        console.log("Delete results:", result);     //To see result
+        return result.rowsAffected[0] === 1;    // Check if a row was deleted
+    }
+
+    static async retrieveReview(reviewId){
+        const connection = await sql.connect(dbConfig);
+        const sqlQuery =`
+        Select * from Reviews
+        Where reviewId = @ReviewId
+        `;
+
+        const  request = connection.request();
+        request.input("ReviewId", sql.VarChar, reviewId);
+
+        const result = await request.query(sqlQuery);
+        connection.close();
+
+        if (result.recordset.length === 0) {
+            return null; // Review not found
+        }
+
+        console.log("Review results:", result);
+        const review = result.recordset[0];
+        return review;
+    }
+
+    static async retrieveReviews(riderId){
+        const connection = await sql.connect(dbConfig);
+        const sqlQuery =`
+        Select * from Reviews
+        Where riderId = @RiderId
+        `;
+
+        const request = connection.request();
+        request.input("RiderId", sql.VarChar, riderId);
+
+        const result = await request.query(sqlQuery);
+        connection.close();
+
+        if (result.recordset.length === 0) {
+            return null; // Reviews not found
+        }
+
+        console.log("Review results:", result);
+        return result.recordset.map(
+            (review) => new Review(
+                review.email,
+                review.name,
+                review.message,
+                review.riderId,
+            )
+        );
+    }
+
+    static async updateReview(message, reviewId){
+        const connection = await sql.connect(dbConfig);
+        const sqlQuery =`
+        Update Reviews
+        SET message = @Message
+        Where reviewId = @ReviewId;
+        `;
+
+        const request = connection.request();
+        request.input("Message", sql.Text, message);
+        request.input("ReviewId", sql.Int, reviewId);
+
+        const result = await request.query(sqlQuery);
+        connection.close();
+
+        console.log("Update results:", result);
+        return result.rowsAffected[0] === 1;
     }
 }
 
